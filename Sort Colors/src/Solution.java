21d4ddb93e69de32004a8d5618c719892f5f7f7f
Xiao Peng <xiao.peng1006@gmail.com>
2020-01-15 10:36:55 -0800

    75. Sort Colors

--- Sort Colors/src/Solution.java
+++ Sort Colors/src/Solution.java
@@ -1,20 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // Using two pointers
-        int l = 0, r = nums.length - 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                nums[l++] = 0;
-            } else if (nums[i] == 2) {
-                r--;
+        // Counting the number of colors
+        int zeroes = 0, ones = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                zeroes++;
+            } else if (num == 1) {
+                ones++;
             }
         }
 
-        for (int i = l; i <= r; i++) {
+        for (int i = 0; i < zeroes; i++) {
+            nums[i] = 0;
+        }
+
+        for (int i = zeroes; i < zeroes + ones; i++) {
             nums[i] = 1;
         }
 
-        for (int i = r + 1; i < nums.length; i++) {
+        for (int i = zeroes + ones; i < nums.length; i++) {
             nums[i] = 2;
         }
     }
