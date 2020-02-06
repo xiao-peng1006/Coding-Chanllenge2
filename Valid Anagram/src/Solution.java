9bf49874fd3caac8f88fb734ed1f6a9d77ea3b78
Xiao Peng <xiao.peng1006@gmail.com>
2020-02-06 10:43:11 -0800

    242. Valid Anagram

---
+++ Valid Anagram/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c:s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c:t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            } else {
+                if (map.get(c) == 1) {
+                    map.remove(c);
+                } else {
+                    map.put(c, map.get(c) - 1);
+                }
+            }
+        }
+        return map.isEmpty();
+    }
+}
