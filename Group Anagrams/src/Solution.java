aaac1ed2218ac7253cc4a1a5978f3c338f4f9c22
Xiao Peng <xiao.peng1006@gmail.com>
2020-02-11 17:24:54 -0800

    49. Group Anagrams

---
+++ Group Anagrams/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedString = String.valueOf(charArray);
+            if (!map.containsKey(sortedString)) {
+                map.put(sortedString, new ArrayList<>());
+            }
+            map.get(sortedString).add(str);
+        }
+
+        for (String str : map.keySet()) {
+            res.add(map.get(str));
+        }
+
+        return res;
+    }
+}
